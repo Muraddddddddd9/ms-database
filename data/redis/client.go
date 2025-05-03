a70bf62688d2843e2387f463a122e0393d819bd1
Murad <abdullaevmurad2221@gmail.com>
2025-05-03 19:28:16 +0300

    use "github.com/redis/go-redis/v9" instead of "github.com/gofiber/storage/redis/v3"

--- data/redis/client.go
+++ data/redis/client.go
@@ -6,11 +6,10 @@ import (
 	"log"
 
 	"github.com/Muraddddddddd9/ms-database/config"
-
-	"github.com/gofiber/storage/redis/v3"
+	"github.com/redis/go-redis/v9"
 )
 
-func Connect() (*redis.Storage, error) {
+func Connect() (*redis.Client, error) {
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal("Failed to load configuration:", err)
@@ -21,17 +20,18 @@ func Connect() (*redis.Storage, error) {
 		return nil, fmt.Errorf("Redis host or port are required")
 	}
 
-	storage := redis.New(redis.Config{
-		Host:     cfg.REDIS_HOST,
-		Port:     cfg.REDIS_PORT,
+	addr := fmt.Sprintf("%s:%d", cfg.REDIS_HOST, cfg.REDIS_PORT)
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
 		Password: cfg.REDIS_PASSWORD,
-		Database: cfg.REDIS_DB,
+		DB:       cfg.REDIS_DB,
 	})
 
-	if _, err := storage.Conn().Ping(context.Background()).Result(); err != nil {
+	if err := client.Ping(context.Background()).Err(); err != nil {
 		log.Fatal("Failed to ping Redis:", err)
 		return nil, err
 	}
 
-	return storage, nil
+	return client, nil
 }
