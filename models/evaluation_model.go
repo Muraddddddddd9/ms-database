78472aa00cbba0ae544113517b59d874dc683893
Muraddddddddd9 <abdullaevmurad2221@gmail.com>
2025-05-06 21:34:41 +0300

    create evaluation model

---
+++ models/evaluation_model.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type EvaluationModel struct {
+	ID      primitive.ObjectID `bson:"_id" json:"_id"`
+	Value   string             `bson:"value" json:"value"`
+	Date    string             `bson:"date" json:"date"`
+	Student primitive.ObjectID `bson:"student" json:"student"`
+	Object  primitive.ObjectID `bson:"object" json:"object"`
+}
+
+type EvaluationModelWithStudent struct {
+	ID      primitive.ObjectID `bson:"_id" json:"_id"`
+	Value   string             `bson:"value" json:"value"`
+	Date    string             `bson:"date" json:"date"`
+	Student string             `bson:"student" json:"student"`
+	Object  string             `bson:"object" json:"object"`
+}
